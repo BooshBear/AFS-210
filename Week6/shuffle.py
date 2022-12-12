ccc985e1f1d2e4ba53b1d18009f577800315bfb9
Dylan <BooshBear03@gmail.com>
2022-12-12 00:30:34 -0800

    week6

--- Week6/shuffle.py
+++ Week6/shuffle.py
@@ -2,7 +2,8 @@ from random import *
 myList = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 
 def ShuffleIt(nlist):
-    # I believe the time complexity of this algorithm would be Logarithmic Time
+    # I believe the time complexity of this algorithm would be Linear Time 
+    # because it increases in time as the array increases
     for i in nlist:
         randomInteger = randrange(0, len(nlist))
         randomPlace = nlist.pop(randomInteger)
